1da4166d50cb35f96094f6607a58a9ace574769d
FishWhisperer <katja.rutnik1@student.um.si>
2019-02-15 19:02:26 +0100

    feat: login activity ui

---
+++ app/src/main/java/si/inova/zimskasola/LoginActivity.kt
@@ -0,0 +1,16 @@
+package si.inova.zimskasola
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.zimskasola.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+
+}
